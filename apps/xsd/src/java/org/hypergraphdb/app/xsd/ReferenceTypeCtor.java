1132530ee5c2b1c94f04e5106b429dda61f9fec5
liaxim <liaxim@43784a7f-e236-0410-9472-c78ae7dffccf>
2007-10-16 03:22:42 +0000

    Started adding support for global elements (references to types).

---
+++ apps/xsd/src/java/org/hypergraphdb/app/xsd/ReferenceTypeCtor.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of the XSD for HyperGraphDB source distribution. This is copyrighted
+ * software. For permitted uses, licensing options and redistribution, please see
+ * the LicensingInformation file at the root level of the distribution.
+ *
+ * Copyright (c) 2007
+ * Kobrix Software, Inc.  All rights reserved.
+ */
+package org.hypergraphdb.app.xsd;
+
+import java.io.UnsupportedEncodingException;
+
+import org.hypergraphdb.HGHandle;
+import org.hypergraphdb.HGHandleFactory;
+import org.hypergraphdb.HGPersistentHandle;
+import org.hypergraphdb.LazyRef;
+import org.hypergraphdb.type.HGAtomTypeBase;
+
+public class ReferenceTypeCtor extends HGAtomTypeBase
+{
+   public static final HGPersistentHandle HANDLE = HGHandleFactory
+         .makeHandle("ba19fe3c-5b32-11db-82a1-a78cc7527afc");
+
+   public Object make(
+      HGPersistentHandle handle, LazyRef<HGHandle[]> targetSet,
+      LazyRef<HGHandle[]> incidenceSet)
+   {
+      ReferenceType result = null;
+      
+      byte[] bytes = graph.getStore().getData(handle);
+      try
+      {
+         String actualType = new String(bytes, "UTF-8");
+         result = new ReferenceType(actualType);
+      } catch (UnsupportedEncodingException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return result;
+   }
+
+   public void release(
+      HGPersistentHandle handle)
+   {
+   }
+
+   public HGPersistentHandle store(
+      Object instance)
+   {
+      HGPersistentHandle result = null;
+      ReferenceType referenceType = (ReferenceType) instance;
+
+      try
+      {
+         byte[] bytes = referenceType.getActualType().getBytes("UTF-8");
+         result = graph.getStore().store(bytes);
+      } catch (UnsupportedEncodingException e)
+      {
+         e.printStackTrace();
+      }
+
+      return result;
+   }
+
+}

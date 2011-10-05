85bd4f0dece2550ce40c8b4460042fc5d6f167c8
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2011-10-05 13:54:47 +0000

    initial add.

---
+++ trunk/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/owl/HGOwlEditorKit.java
@@ -0,0 +1,217 @@
+package gov.miamidade.hgowl.plugin.owl;
+
+import gov.miamidade.hgowl.plugin.owl.model.HGOwlModelManagerImpl;
+import gov.miamidade.hgowl.plugin.ui.CreateHGOntologyWizard;
+
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.Logger;
+import org.hypergraphdb.app.owl.HGDBOntologyManager;
+import org.protege.editor.core.editorkit.EditorKit;
+import org.protege.editor.core.editorkit.EditorKitDescriptor;
+import org.protege.editor.core.ui.wizard.Wizard;
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.OWLEditorKitFactory;
+import org.protege.editor.owl.ProtegeOWL;
+import org.protege.editor.owl.model.OWLModelManager;
+import org.protege.editor.owl.model.OWLModelManagerImpl;
+import org.protege.editor.owl.model.SaveErrorHandler;
+import org.protege.editor.owl.ui.error.OntologyLoadErrorHandlerUI;
+import org.protege.editor.owl.ui.explanation.ExplanationManager;
+import org.protege.editor.owl.ui.ontology.imports.missing.MissingImportHandlerUI;
+import org.protege.editor.owl.ui.ontology.wizard.create.CreateOntologyWizard;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.util.VersionInfo;
+
+/**
+ * HGOwlEditorKit
+ * Here, the crucial connection between Protege Editor and our OWL Api 
+ * with Hypergraph backend is established (see initialize). 
+ * The connection point to the Hypergraph OWL-API implementation is HGOWLModelManager.
+ * 
+ * @author Thomas Hilpold
+ */
+public class HGOwlEditorKit extends OWLEditorKit {
+
+	private static final Logger logger = Logger.getLogger(HGOwlEditorKit.class);
+
+    public static final String ID = "HGOwlEditorKit";
+
+    public HGOwlEditorKit(OWLEditorKitFactory editorKitFactory) {
+		super(editorKitFactory);
+	}
+		
+
+    protected void initialise(){
+    	// DO NOT DO THIS: super.initialise();    	
+    	// THIS SETS OUR MODEL MANAGER
+    	HGOwlModelManagerImpl modelManager = new HGOwlModelManagerImpl();
+    	setOWLModelManager(modelManager);
+        logger.info("Using OWL API version " + VersionInfo.getVersionInfo().getVersion());
+        this.newPhysicalURIs = new HashSet<URI>();
+        modelManager.setExplanationManager(new ExplanationManager(this));
+        modelManager.setMissingImportHandler(new MissingImportHandlerUI(this));
+        modelManager.setSaveErrorHandler(new SaveErrorHandler(){
+            public void handleErrorSavingOntology(OWLOntology ont, URI physicalURIForOntology, OWLOntologyStorageException e) throws Exception {
+                handleSaveError(ont, physicalURIForOntology, e);
+            }
+        });
+        loadErrorHandler = new OntologyLoadErrorHandlerUI(this);
+        modelManager.setLoadErrorHandler(loadErrorHandler);
+        loadIOListenerPlugins();
+        registration = ProtegeOWL.getBundleContext().registerService(EditorKit.class.getCanonicalName(), this, new Properties());
+
+    }
+
+    protected void initialiseCompleted() {
+        super.initialiseCompleted();
+    }
+    
+
+    /**
+     * Gets the <code>EditorKit</code> Id.  This can be used to identify
+     * the type of <code>EditorKit</code>.
+     * @return A <code>String</code> that represents the <code>EditorKit</code>
+     *         Id.
+     */
+    public String getId() {
+        return ID;
+    }
+
+    public boolean handleNewRequest() throws Exception {
+    	boolean handleNewSuccess = false;
+    	CreateHGOntologyWizard w = new CreateHGOntologyWizard(null, this);
+        int result = w.showModalDialog();
+        if (result == Wizard.FINISH_RETURN_CODE) {
+            OWLOntologyID oid = w.getOntologyID();
+            if (oid != null) {
+            	HGOwlModelManagerImpl mm = (HGOwlModelManagerImpl)getOWLModelManager();
+            	mm.createNewOntology(oid, w.getLocationURI());
+            	//addToRecent(URI.create(prop.getProperty("hibernate.connection.url")));
+            	addRecent(w.getLocationURI());
+        		handleNewSuccess = true;
+            }
+        }
+        return handleNewSuccess;
+    }
+
+    public boolean handleLoadRecentRequest(EditorKitDescriptor descriptor) throws Exception {
+    	System.out.println("HG handleLoadRecentRequest");
+        return super.handleLoadRecentRequest(descriptor );
+    }
+
+    public boolean handleLoadRequest() throws Exception {
+    	System.out.println("HG HandleLoadRequest");
+    	return super.handleLoadRequest();
+//    	if(DatabaseDialogPanel.showDialog(this)){
+//    		Properties prop = getDBProperty();
+//    		boolean success =  handleLoadFrom(prop);
+//    		if(success == true)
+//    			addToRecent(URI.create(prop.getProperty("hibernate.connection.url")));
+//    		return success;
+//    	}
+//    	else
+//    		return false;
+    }
+
+    public boolean handleLoadFrom(URI uri) throws Exception {    	
+        boolean success = ((HGOwlModelManagerImpl) getModelManager()).loadOntologyFromPhysicalURI(uri);
+        if (success){
+            addRecent(uri);
+        }
+        return success;
+    }
+
+    public void handleSave() throws Exception {
+    	System.out.println("HG handleSave ");
+    	super.handleSave();
+    	OWLOntology ont = getModelManager().getActiveOntology();
+//    	OWLOntologyFormat format = getModelManager().getOWLOntologyManager().getOntologyFormat(ont);
+//    	// if the format is Database, do nothing because is is already saved
+//    	if(format instanceof OWLDBOntologyFormat){
+//    		return;
+//    	}
+//    	
+//        try {
+//            getModelManager().save();
+//            getWorkspace().save();
+//            for (URI uri : newPhysicalURIs) {
+//                addRecent(uri);
+//            }
+//            newPhysicalURIs.clear();
+//        }
+//        catch (OWLOntologyStorerNotFoundException e) {
+//            ont = getModelManager().getActiveOntology();
+//            format = getModelManager().getOWLOntologyManager().getOntologyFormat(ont);
+//            String message = "Could not save ontology in the specified format (" + format + ").\n" + "Please selected 'Save As' and select another format.";
+//            logger.warn(message);
+//            JOptionPane.showMessageDialog(getWorkspace(),
+//                                          message,
+//                                          "Could not save ontology",
+//                                          JOptionPane.ERROR_MESSAGE);
+//        }
+    }
+
+
+
+    public void handleSaveAs() throws Exception {
+    	System.out.println("HG handleSaveAs ");
+    	super.handleSaveAs();
+    	
+//        OWLOntologyManager man = getModelManager().getOWLOntologyManager();
+//        OWLOntology ont = getModelManager().getActiveOntology();
+//        OWLOntologyFormat format = DBOntologyFormatPanel.showDialog(this, man.getOntologyFormat(ont));
+//        if (format == null) {
+//            logger.warn("Please select a valid format");
+//            return;
+//        }
+//        if(format instanceof OWLDBOntologyFormat){
+//        	if(DatabaseDialogPanel.showDialog(this)){
+//        		Properties prop = getDBProperty();
+//        		((HGModelManagerImpl)modelManager).saveAsDB(prop);
+//        	}
+//        	
+//        	
+//        } else {
+//        	UIHelper helper = new UIHelper(this);
+//            File file = helper.saveOWLFile("Please select a location in which to save: " + getModelManager().getRendering(ont));
+//            if (file != null) {
+//                int extensionIndex = file.toString().lastIndexOf('.');
+//                if (extensionIndex == -1) {
+//                    file = new File(file.toString() + ".owl");
+//                }
+//                else if (extensionIndex != file.toString().length() - 4) {
+//                    file = new File(file.toString() + ".owl");
+//                }
+//            }
+//            if (file != null){
+//                man.setOntologyFormat(ont, format);
+//                IRI documentIRI = IRI.create(file.toURI());
+//                man.setOntologyDocumentIRI(ont, documentIRI);
+//                getModelManager().setDirty(ont);
+//                newPhysicalURIs.add(file.toURI());
+//                handleSave();
+//            }
+//            else{
+//                logger.warn("No valid file specified for the save as operation - quitting");
+//            }
+//        }
+        
+    }
+    
+    public void dispose() {
+        super.dispose();
+        HGOwlModelManagerImpl m = (HGOwlModelManagerImpl)getOWLModelManager();
+        //m.get        
+    }
+       
+
+}
---
+++ trunk/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/owl/HGOwlOntologyRepository.java
@@ -0,0 +1,157 @@
+package gov.miamidade.hgowl.plugin.owl;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.hypergraphdb.app.owl.HGDBOntology;
+import org.hypergraphdb.app.owl.HGDBOntologyRepository;
+import org.protege.editor.core.OntologyRepository;
+import org.protege.editor.core.OntologyRepositoryEntry;
+import org.protege.editor.core.editorkit.EditorKit;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntologyIRIMapper;
+import org.semanticweb.owlapi.util.OntologyIRIShortFormProvider;
+
+/**
+ * A protege repository implementation backed by a Hypergraph Ontology Repository instance.
+ * This enabled Protege to show Hypergraph ontologies at startup. 
+ * 
+ * @author Thomas Hilpold
+ */
+public class HGOwlOntologyRepository implements OntologyRepository {
+
+    private String repositoryName;
+
+    private HGDBOntologyRepository dbRepository;
+
+    private List<RepositoryEntry> entries;
+
+    private OWLOntologyIRIMapper iriMapper;
+
+
+    public HGOwlOntologyRepository(String repositoryName, HGDBOntologyRepository dbRepository) {
+        this.repositoryName = repositoryName;
+        this.dbRepository = dbRepository;
+        entries = new ArrayList<RepositoryEntry>();
+        iriMapper = new RepositoryIRIMapper();
+    }
+
+
+    public void initialise() throws Exception {
+    }
+
+
+    public String getName() {
+        return repositoryName;
+    }
+
+
+    public String getLocation() {
+        return "Hypergraph Repository at " + dbRepository.HYPERGRAPH_DB_LOCATION;
+    }
+
+
+    public void refresh() {
+        fillRepository();
+    }
+
+
+    public Collection<OntologyRepositoryEntry> getEntries() {
+        List<OntologyRepositoryEntry> ret = new ArrayList<OntologyRepositoryEntry>();
+        ret.addAll(entries);
+        return ret;
+    }
+
+
+    public List<Object> getMetaDataKeys() {
+        return Collections.emptyList();
+    }
+
+
+    public void dispose() throws Exception {
+    	dbRepository.dispose();
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Implementation details
+
+
+    private void fillRepository() {
+        entries.clear();
+        List<HGDBOntology>  l = dbRepository.getOntologies();
+        for(HGDBOntology o : l) {
+            entries.add(new RepositoryEntry(o));
+        }
+    }
+
+    private class RepositoryEntry implements OntologyRepositoryEntry {
+
+        private String shortName;
+
+        private URI ontologyURI;
+
+        private URI physicalURI;
+
+        public RepositoryEntry(HGDBOntology o) {
+        	this.shortName = o.getOntologyID().getOntologyIRI().getFragment();
+            this.ontologyURI = URI.create(o.getOntologyID().getOntologyIRI().toString());
+            OntologyIRIShortFormProvider sfp = new OntologyIRIShortFormProvider();
+            shortName = sfp.getShortForm(o);
+            physicalURI = URI.create(o.getDocumentIRI().toString());
+        }
+
+
+        public String getOntologyShortName() {
+            return shortName;
+        }
+
+
+        public URI getOntologyURI() {
+            return ontologyURI;
+        }
+
+
+        public URI getPhysicalURI() {
+            return physicalURI;
+        }
+
+
+        public String getEditorKitId() {
+            return HGOwlEditorKitFactory.ID;
+        }
+
+
+        public String getMetaData(Object key) {
+            return null;
+        }
+
+
+        public void configureEditorKit(EditorKit editorKit) {
+            ((HGOwlEditorKit) editorKit).getOWLModelManager().getOWLOntologyManager().addIRIMapper(iriMapper);
+        }
+
+
+        public void restoreEditorKit(EditorKit editorKit) {
+            ((HGOwlEditorKit) editorKit).getOWLModelManager().getOWLOntologyManager().removeIRIMapper(iriMapper);
+
+        }
+    }
+
+
+    private class RepositoryIRIMapper implements OWLOntologyIRIMapper {
+
+        public IRI getDocumentIRI(IRI iri) {
+            for(RepositoryEntry entry : entries) {
+                if(entry.getOntologyURI().equals(iri.toURI())) {
+                    return IRI.create(entry.getPhysicalURI());
+                }
+            }
+            return null;
+        }
+    }
+
+}

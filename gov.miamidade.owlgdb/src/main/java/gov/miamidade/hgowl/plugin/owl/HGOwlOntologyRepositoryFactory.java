07da9f289dda7add43e5ce0500193e086426ea96
thomas.hilpold <thomas.hilpold@gmail.com>
2011-10-05 13:54:47 +0000

    initial add.

---
+++ gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/owl/HGOwlOntologyRepositoryFactory.java
@@ -0,0 +1,39 @@
+package gov.miamidade.hgowl.plugin.owl;
+
+import org.hypergraphdb.app.owl.HGDBOntologyRepository;
+import org.protege.editor.core.OntologyRepository;
+import org.protege.editor.core.OntologyRepositoryFactory;
+
+/**
+ * HGOwlOntologyRepositoryFactory.
+ * A factory for Hypergraph backed Protege OntologyRepositories.
+ * (Initialise doesn't get called, it seems) 
+ * 
+ * @author Thomas Hilpold (GIC/Miami-Dade County)
+ * @created Sep 26, 2011
+ */
+public class HGOwlOntologyRepositoryFactory extends OntologyRepositoryFactory {
+
+	HGDBOntologyRepository dbRepository;
+	
+	@Override
+	public void initialise() throws Exception {
+		dbRepository = new HGDBOntologyRepository();
+		 
+	}
+
+	@Override
+	public void dispose() throws Exception {
+		dbRepository.dispose();
+		dbRepository = null;
+	}
+
+	@Override
+	public OntologyRepository createRepository() {
+		if (dbRepository == null) {
+			dbRepository = new HGDBOntologyRepository();
+		}
+		return new HGOwlOntologyRepository("Hypergraph", dbRepository);
+	}
+
+}

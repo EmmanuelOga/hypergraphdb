f41af0d87ce3dd524397ceb908eeaa32b3133be9
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2011-12-06 20:58:07 +0000

    Added Metadata Keys physicalURI, axiomcount, atomcount.

--- trunk/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/owl/HGOwlOntologyRepository.java
+++ trunk/gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/owl/HGOwlOntologyRepository.java
@@ -2,10 +2,11 @@ package gov.miamidade.hgowl.plugin.owl;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
+import org.hypergraphdb.HGQuery.hg;
 import org.hypergraphdb.app.owl.HGDBOntology;
 import org.hypergraphdb.app.owl.HGDBOntologyRepository;
 import org.protege.editor.core.OntologyRepository;
@@ -23,6 +24,13 @@ import org.semanticweb.owlapi.util.OntologyIRIShortFormProvider;
  */
 public class HGOwlOntologyRepository implements OntologyRepository {
 
+	public static final String PHYSICAL_URI = "Physical URI";
+	public static final String AXIOM_COUNT = "Axiom Count";
+	public static final String ATOM_COUNT = "Atom Count";
+	
+	public static final List<Object> METADATA_KEYS = Arrays.asList(new Object[]{PHYSICAL_URI, AXIOM_COUNT, ATOM_COUNT});
+	
+	
     private String repositoryName;
 
     private HGDBOntologyRepository dbRepository;
@@ -60,7 +68,7 @@ public class HGOwlOntologyRepository implements OntologyRepository {
     }
 
     public List<Object> getMetaDataKeys() {
-        return Collections.emptyList();
+        return METADATA_KEYS;
     }
 
     public void dispose() throws Exception {
@@ -87,43 +95,62 @@ public class HGOwlOntologyRepository implements OntologyRepository {
 
         private URI physicalURI;
 
+        private int nrOfAxioms;
+
+		private int nrOfAtoms;
+
         public RepositoryEntry(HGDBOntology o) {
         	this.shortName = o.getOntologyID().getOntologyIRI().getFragment();
             this.ontologyURI = URI.create(o.getOntologyID().getOntologyIRI().toString());
             OntologyIRIShortFormProvider sfp = new OntologyIRIShortFormProvider();
             shortName = sfp.getShortForm(o);
             physicalURI = URI.create(o.getDocumentIRI().toString());
+            nrOfAxioms = o.getAxiomCount();
+            nrOfAtoms = (int)o.count(hg.all());
         }
-
 
         public String getOntologyShortName() {
             return shortName;
         }
 
-
         public URI getOntologyURI() {
             return ontologyURI;
         }
-
 
         public URI getPhysicalURI() {
             return physicalURI;
         }
 
+		public int getNrOfAxioms() {
+			return nrOfAxioms;
+		}
+
+		/**
+		 * @return the nrOfAtoms that are in the ontology subgraph.
+		 */
+		public int getNrOfAtoms() {
+			return nrOfAtoms;
+		}
 
         public String getEditorKitId() {
             return HGOwlEditorKitFactory.ID;
         }
 
-
         public String getMetaData(Object key) {
-            return null;
+        	if (key.equals(PHYSICAL_URI)) {
+        		return "" + getPhysicalURI();
+        	} else if (key.equals(AXIOM_COUNT)) {
+        		return "" + getNrOfAxioms();
+        	} else if (key.equals(ATOM_COUNT)) {
+        		return "" + getNrOfAtoms();
+        	} else {
+        		throw new IllegalArgumentException("Key unknown.");
+        	}
         }
 
         public void configureEditorKit(EditorKit editorKit) {
             ((HGOwlEditorKit) editorKit).getOWLModelManager().getOWLOntologyManager().addIRIMapper(iriMapper);
         }
-
 
         public void restoreEditorKit(EditorKit editorKit) {
             ((HGOwlEditorKit) editorKit).getOWLModelManager().getOWLOntologyManager().removeIRIMapper(iriMapper);

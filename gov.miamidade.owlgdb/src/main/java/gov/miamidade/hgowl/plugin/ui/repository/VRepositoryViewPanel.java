82b1dd2a950e060dee58597ee4546895f3e446fd
thomas.hilpold <thomas.hilpold@gmail.com>
2012-01-31 21:34:17 +0000

    upgraded to HGOntologyRepositoryEntry

--- gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/ui/repository/VRepositoryViewPanel.java
+++ gov.miamidade.owlgdb/src/main/java/gov/miamidade/hgowl/plugin/ui/repository/VRepositoryViewPanel.java
@@ -1,5 +1,7 @@
 package gov.miamidade.hgowl.plugin.ui.repository;
 
+import gov.miamidade.hgowl.plugin.owl.model.HGOntologyRepositoryEntry;
+
 import javax.swing.JOptionPane;
 
 import org.protege.editor.core.OntologyRepository;
@@ -22,12 +24,12 @@ public class VRepositoryViewPanel extends RepositoryViewPanel {
 		super(repository);
 	}
 	
-    public static OntologyRepositoryEntry showAddToVersionControlDialog(OntologyRepository repository) {
+    public static HGOntologyRepositoryEntry showAddToVersionControlDialog(OntologyRepository repository) {
         repository.refresh();
         RepositoryViewPanel panel = new RepositoryViewPanel(repository);
         int ret = JOptionPaneEx.showConfirmDialog(null, "Add Ontology to Version control (" + repository.getName() + ")", panel, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION, panel.getTable());
         if(ret == JOptionPane.OK_OPTION) {
-        	OntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
+        	HGOntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
         	if (ore == null) {
         		JOptionPane.showMessageDialog(null,
         	        "You did not select an ontology.",
@@ -39,12 +41,12 @@ public class VRepositoryViewPanel extends RepositoryViewPanel {
         return null;
     }
 
-    public static OntologyRepositoryEntry showRemoveFromVersionControlDialog(OntologyRepository repository) {
+    public static HGOntologyRepositoryEntry showRemoveFromVersionControlDialog(OntologyRepository repository) {
         repository.refresh();
         RepositoryViewPanel panel = new RepositoryViewPanel(repository);
         int ret = JOptionPaneEx.showConfirmDialog(null, "Remove Ontology from Version control (" + repository.getName() + ")", panel, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION, panel.getTable());
         if(ret == JOptionPane.OK_OPTION) {
-        	OntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
+        	HGOntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
         	if (ore == null) {
         		JOptionPane.showMessageDialog(null,
         	        "You did not select an ontology.",
@@ -56,12 +58,12 @@ public class VRepositoryViewPanel extends RepositoryViewPanel {
         return null;
     }
 
-    public static OntologyRepositoryEntry showCommitDialog(OntologyRepository repository) {
+    public static HGOntologyRepositoryEntry showCommitDialog(OntologyRepository repository) {
         repository.refresh();
         RepositoryViewPanel panel = new RepositoryViewPanel(repository);
         int ret = JOptionPaneEx.showConfirmDialog(null, "Commit Ontology - Version control (" + repository.getName() + ")", panel, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION, panel.getTable());
         if(ret == JOptionPane.OK_OPTION) {
-        	OntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
+        	HGOntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
         	if (ore == null) {
         		JOptionPane.showMessageDialog(null,
         	        "You did not select an ontology.",
@@ -73,12 +75,12 @@ public class VRepositoryViewPanel extends RepositoryViewPanel {
         return null;
     }
 
-    public static OntologyRepositoryEntry showRollbackDialog(OntologyRepository repository) {
+    public static HGOntologyRepositoryEntry showRollbackDialog(OntologyRepository repository) {
         repository.refresh();
         RepositoryViewPanel panel = new RepositoryViewPanel(repository);
         int ret = JOptionPaneEx.showConfirmDialog(null, "Rollback Ontology - Version control (" + repository.getName() + ")", panel, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION, panel.getTable());
         if(ret == JOptionPane.OK_OPTION) {
-        	OntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
+        	HGOntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
         	if (ore == null) {
         		JOptionPane.showMessageDialog(null,
         	        "You did not select an ontology.",
@@ -90,12 +92,12 @@ public class VRepositoryViewPanel extends RepositoryViewPanel {
         return null;
     }
 
-    public static OntologyRepositoryEntry showRevertOneDialog(OntologyRepository repository) {
+    public static HGOntologyRepositoryEntry showRevertOneDialog(OntologyRepository repository) {
         repository.refresh();
         RepositoryViewPanel panel = new RepositoryViewPanel(repository);
         int ret = JOptionPaneEx.showConfirmDialog(null, "Revert Ontology by one revision - Version control (" + repository.getName() + ")", panel, JOptionPane.WARNING_MESSAGE, JOptionPane.OK_CANCEL_OPTION, panel.getTable());
         if(ret == JOptionPane.OK_OPTION) {
-        	OntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
+        	HGOntologyRepositoryEntry ore = panel.getTable().getSelectedEntry(); 
         	if (ore == null) {
         		JOptionPane.showMessageDialog(null,
         	        "You did not select an ontology.",
@@ -106,5 +108,4 @@ public class VRepositoryViewPanel extends RepositoryViewPanel {
         }
         return null;
     }
-
 }
